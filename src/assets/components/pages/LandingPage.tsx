451a2a787397fcc42ac8c18c067bc0ace9414c88
Yonatan Lisanu <166075671+YLB1997@users.noreply.github.com>
2025-10-08 08:58:39 +0300

    Fix background gradient in LandingPage component

--- src/assets/components/pages/LandingPage.tsx
+++ src/assets/components/pages/LandingPage.tsx
@@ -12,7 +12,7 @@ function LandingPage() {
       <main
         className="pt-32 min-h-screen relative overflow-hidden"
         style={{
-          backgroundImage: `linear-gradient(to bottom right, rgba(250, 204, 21, 0.3), rgba(34, 197, 94, 0.3), rgba(0, 0, 0, 0.3))`,
+          backgroundImage: `linear-gradient(to bottom right, rgba(15, 203, 43, 0.3), rgba(34, 197, 94, 0.3), rgba(0, 0, 0, 0.3))`,
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center",
